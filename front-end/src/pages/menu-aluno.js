43d0e63828025175f839d40601802951005ed311
Isabelle Vieira <49079428+isabellevieirab@users.noreply.github.com>
2022-11-30 14:44:53 -0300

    melhorias no get professores

--- front-end/src/pages/menu-aluno.js
+++ front-end/src/pages/menu-aluno.js
@@ -5,20 +5,13 @@ import "./css/styles.css" ;
 import './css/fullcalendar.css';
 import './css/fullcalendar.print.css'; 
 
-import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
-import Row from 'react-bootstrap/Row';
 import Tab from 'react-bootstrap/Tab';
 import $ from 'jquery';
 import Calendario from './js/calendario.js';
 
-/*import './js/jquery-1.10.2.js' ;
-import './js/jquery-ui.custom.min.js';
-import './js/fullcalendar.js';
-import "./js/scripts.js";
-import "./js/calendario-menu.js";
-import "./js/requisicoes.js";
-*/
+
+const i = 0;
 
 
 class MenuAluno extends React.Component{
@@ -29,7 +22,6 @@ class MenuAluno extends React.Component{
         super(props);
         this.state = { apiResponse: "" };
     }
-
     callAPI() {
         fetch("http://localhost:9000/testeAPI")
             .then(res => res.text())
@@ -46,7 +38,7 @@ class MenuAluno extends React.Component{
                 $.each(data, function(index, element) {   
                     $.each(element, function(i, e){
                         // Exibe todas as materias em Menu -> Matérias        
-                        $('#todas-materias').append($('<div class="col"><div id='+e.id+' class="card"><img src="https://64.media.tumblr.com/8b69b19c86e321c791340f370165ba15/tumblr_nvge9wpGCU1u66uqto1_1280.jpg" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'+e.materia+'</h5></div><div class="card-footer">Agendar</div></div></div>'));
+                        $('#todas-materias').append($('<div class="col"><div id='+e.id+' class="card"><img src="https://64.media.tumblr.com/8b69b19c86e321c791340f370165ba15/tumblr_nvge9wpGCU1u66uqto1_1280.jpg" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'+e.materia+'</h5></div><div class="card-footer"><a>Agendar</a></div></div></div>'));
                         // Exibe as três primeiras matérias em Menu -> Início
                         if(i < 3)
                             $('#tres-primeiras-materias').append($('<div class="col"><div id='+e.id+' class="card"><img src="https://64.media.tumblr.com/8b69b19c86e321c791340f370165ba15/tumblr_nvge9wpGCU1u66uqto1_1280.jpg" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'+e.materia+'</h5></div><div class="card-footer">Agendar</div></div></div>'));
@@ -70,56 +62,58 @@ class MenuAluno extends React.Component{
         });
     }
  
-
     retornaPerfis(){
         //Retorna os professores cadastrados
-        var ids = [1,2,3,4]
-        for (var i=0; i<10; i++){
-            $.ajax({
-                url: 'http://localhost:9000/perfil/'+i, 
-                type: 'GET', 
-                dataType: 'json', 
-                success: function(data) {
-                    $.each(data, function(index, element) {   
-                        $.each(element, function(ind, e){
-                            // Exibe todas as materias em Menu -> Matérias        
+        $.ajax({
+            url: 'http://localhost:9000/perfil/id', 
+            type: 'GET', 
+            dataType: 'json', 
+            success: function(data) {
+                var i = 0;
+                $.each(data, function(index, element) {   
+                    $.each(element, function(ind, e){
+                        if(e.isprofessor){
+                            console.log(i)
+                            console.log(e)
                             $('#todos-professores').append($('<div class="col"><div id='+e.id+' class="card"><img src="https://64.media.tumblr.com/8b69b19c86e321c791340f370165ba15/tumblr_nvge9wpGCU1u66uqto1_1280.jpg" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'+e.nome+'</h5></div><div class="card-footer">Agendar</div></div></div>'));
-                            // Exibe as três primeiras matérias em Menu -> Início
-                            if(ind < 3)
+                            // Exibe os três primeiros profs em Menu -> Início
+                            if(i < 3){
                                 $('#tres-primeiros-professores').append($('<div class="col"><div id='+e.id+' class="card"><img src="https://64.media.tumblr.com/8b69b19c86e321c791340f370165ba15/tumblr_nvge9wpGCU1u66uqto1_1280.jpg" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'+e.nome+'</h5></div><div class="card-footer">Agendar</div></div></div>'));
-                            //data-format:  '2011-05-05'
                             
-                        });
-                    }); 
-                    $('.item.active').css('border-color', '');
-                    $('.card').css('border', '')
-                    $('.card').addClass('bg-primario');
-                    $('.card').css('color','white');            
-                    $('.card-footer').addClass('bg-roxo');
-                    $('.card-footer').css('font-size','15px');
-                    $('.card-footer').addClass('text-center');
-                    
-                    //$('#calendario-menu-inicio').fullCalendar({event: eventos});
-                    
-                }, error: function(e) { 
-                    //alert(e); 
-                }
-            });
-        }
+                            }//data-format:  '2011-05-05'
+                            i++;
+                        }
+                    });
+                }); 
+                $('.item.active').css('border-color', '');
+                $('.card').css('border', '')
+                $('.card').addClass('bg-primario');
+                $('.card').css('color','white');            
+                $('.card-footer').addClass('bg-roxo');
+                $('.card-footer').css('font-size','15px');
+                $('.card-footer').addClass('text-center');
+                
+                //$('#calendario-menu-inicio').fullCalendar({event: eventos});
+                
+            }, error: function(e) { 
+                //alert(e); 
+            }
+        });
+    
     }
 
     componentDidMount() {
+        if (this.first) return; this.first = true;
         this.retornaMaterias();
         this.retornaPerfis();
-
     }
-
-    
+  
 
     render(){
-        return (
+        return (        
             <div className="d-flex" id="wrapper">
-                <Tab.Container id="left-tabs-example" defaultActiveKey="tab-inicio">
+                
+               <Tab.Container id="left-tabs-example" defaultActiveKey="tab-inicio">
                 <div className="border-end border-dark text-white bg-primario"  id="sidebar-wrapper">
                     <div className="sidebar-heading border-bottom border-dark py-5">Marcaê</div>
                     <Nav className="list-group list-group-flush " role="tablist" >
